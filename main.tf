eceefb1449a462b5395a8de1ce6582028c0fa36e
Hemanth B <hemanth22hemu@gmail.com>
2022-07-10 19:22:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "azurerm_virtual_machine_extension" "jenkins" {
 
   settings = <<SETTINGS
     {
-        "commandToExecute": "wget -O azurejenkins.sh https://git.io/AzureJenkins && sh azurejenkins.sh"
+        "commandToExecute": "echo jenkins"
     }
 SETTINGS
 
